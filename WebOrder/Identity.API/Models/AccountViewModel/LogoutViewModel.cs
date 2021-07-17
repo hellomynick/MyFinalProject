504c7db4f161ac2412a6b4bcc5181e443fb292d6
unknown <vutvmgcd18517@fpt.edu.vn>
2021-07-17 15:08:18 +0700

    Seed Data

---
+++ WebOrder/Identity.API/Models/AccountViewModel/LogoutViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Models.AccountViewModel
+{
+    public record LogoutViewModel
+    {
+        public string LogoutId { get; set; }
+    }
+}
---
+++ WebOrder/Identity.API/Models/ManagerViewModel/ChangePasswordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Models.ManagerViewModel
+{
+    public record ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; init; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; init; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; init; }
+    }
+}
